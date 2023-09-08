2c32eaa8ebadb8be7cbae7dc9911e6fe5feea677
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-09-08 10:37:37 -0400

    passed partially

--- server/app.py
+++ server/app.py
@@ -26,16 +26,85 @@ def messages():
             message_dict = message.to_dict()
             messages.append(message_dict)
 
-    response = make_response(
+        response = make_response(
         messages,
         200
     )
+        return response
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.form.get("body"),
+            username=request.form.get("username"),
+                    )
 
-    return response
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            message_dict,
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if message == None:
+        response_body = {
+            "message": "This message does not exist in our database. Please try again."
+        }
+        response = make_response(jsonify(response_body), 404)
+
+        return response
+
+    else:
+        if request.method == 'GET':
+            message_dict = message.to_dict()
+
+            response = make_response(
+                message_dict,
+                200
+            )
+
+            return response
+
+        elif request.method == 'PATCH':
+            message = Message.query.filter(Message.id == id).first()
+
+            message.body = request.form.get("body")
+
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict = message.to_dict()
+
+            response = make_response(
+                message_dict,
+                200
+            )
+
+            return response
+
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Review deleted."    
+            }
+
+            response = make_response(
+                response_body,
+                200
+            )
+
+            return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
