5d513e8f59582cd38b526806b126385352574a44
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-09-08 09:38:53 -0400

    fixes created time

--- server/models.py
+++ server/models.py
@@ -11,4 +11,15 @@ db = SQLAlchemy(metadata=metadata)
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
+    # serialize_rules = ('-reviews.game',)
+
     id = db.Column(db.Integer, primary_key=True)
+    body= db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # reviews = db.relationship('Review', backref='game')
+
+    def __repr__(self):
+        return f'<Message {self.body} for {self.username}>'
